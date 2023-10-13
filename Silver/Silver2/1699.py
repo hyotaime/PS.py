db238a555d79994701f2a525cd67aeb850a357e7
hyotaime <hyotaime@hyotaime.com>
2023-10-13 10:02:02 +0900

    231013: 1699

---
+++ Silver/Silver2/1699.py
@@ -0,0 +1,14 @@
+# Python TLE
+# PyPy3 AC
+import sys
+
+input = sys.stdin.readline
+n = int(input())
+dp = [0] * (n + 1)
+for i in range(1, n + 1):
+    dp[i] = i
+    j = 1
+    while j * j <= i:
+        dp[i] = min(dp[i], dp[i - j * j] + 1)
+        j += 1
+print(dp[n])

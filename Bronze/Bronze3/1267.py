142229f78e750333756266993bc1fb9a2d5f1244
hyotaime <hyotaime@hyotaime.com>
2023-12-04 12:48:42 +0900

    231204

---
+++ Bronze/Bronze3/1267.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+n = int(input())
+arr = list(map(int, input().split()))
+y, m = 0, 0
+for i in arr:
+    y += (i // 30 + 1) * 10
+    m += (i // 60 + 1) * 15
+if y < m:
+    print('Y', y)
+elif y > m:
+    print('M', m)
+else:
+    print('Y', 'M', y)

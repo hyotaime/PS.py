9da9f43eed72efa2818c319dff9c47bbef0efda5
hyotaime <hyotaime@hyotaime.com>
2023-12-23 12:40:49 +0900

    231223

---
+++ Bronze/Bronze3/23812.py
@@ -0,0 +1,36 @@
+import sys
+
+input = sys.stdin.readline
+n = int(input())
+for i in range(n):
+    for j in range(n):
+        print('@', end='')
+    for j in range(n):
+        print('   ', end='')
+    for j in range(n):
+        print('@', end='')
+    print()
+for i in range(n):
+    for j in range(n):
+        print('@@@@@', end='')
+    print()
+for i in range(n):
+    for j in range(n):
+        print('@', end='')
+    for j in range(n):
+        print('   ', end='')
+    for j in range(n):
+        print('@', end='')
+    print()
+for i in range(n):
+    for j in range(n):
+        print('@@@@@', end='')
+    print()
+for i in range(n):
+    for j in range(n):
+        print('@', end='')
+    for j in range(n):
+        print('   ', end='')
+    for j in range(n):
+        print('@', end='')
+    print()

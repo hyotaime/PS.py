814fe5e8b4251fa67c831cf4b3c40b0545677023
hyotaime <hyotaime@hyotaime.com>
2024-01-22 20:45:35 +0900

    240122

---
+++ Bronze/Bronze3/11368.py
@@ -0,0 +1,8 @@
+import sys
+
+input = sys.stdin.readline
+while True:
+    c, w, l, p = map(int, input().split())
+    if c == 0 and w == 0 and l == 0 and p == 0:
+        break
+    print(((c ** w) ** l) ** p)
